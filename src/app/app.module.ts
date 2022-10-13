f172971cf26d1e5cf9b63cee53c110b60a701c7f
YanBarbosaLouzada <yan.barbosalouzada@gmail.com>
2022-10-13 15:38:42 -0300

    Alteração dia 13/10/22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProdutoComponent } from './produto/produto.component';
 
 @NgModule({
+
+  /**
+   * 
+   * declarations serve para registrar 
+   * componentes, pipes, diretivas e outra estruturas
+   */
+
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoComponent
   ],
+
+  /**
+   * imports serve para registra módulos dentro de outros módulos
+   * 
+   * SERVE EXCLUSIVAMNETE PARA OUTROS MÓDULOS
+   */
+
   imports: [
     BrowserModule
   ],
+  /**
+   * providers serve para registrar serviços e interceptadores 
+   * HTTP
+   */
+
   providers: [],
+
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
---
+++ src/app/produto/produto.component.ts
@@ -0,0 +1,22 @@
+
+ // Todo componente do Angula é representado por uma classe.
+ 
+
+// Exportando uma classe
+    
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'card-produto',
+    templateUrl:'./produto.component.html',
+    styleUrls: ['./produto.component.css']
+})
+
+export class ProdutoComponent{
+
+}
+
+
+
+
+
